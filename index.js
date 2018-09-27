9bf365f3ca9be7f0341693ff613032b08e34ec69
Jack Ling <j.lingyx@gmail.com>
2018-09-27 13:57:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 function dwarfRollCall(dwarves) {
+  var dwarfList = []
+  for (let i = 0; i < dwarves.length; i++){
+    dwarfList.push(`${i+1}. ${dwarves[i]} `)
+    
+  }
+  return dwarfList.join("")
 }
 
+var planetCaps = []
 function summonCaptainPlanet(planeteerCalls){
+  for (let i = 0; i<planeteerCalls.length; i++){
+  
+  planetCaps.push(`${planeteerCalls[i].toUpperCase}!`)
+}
+  return planetCaps
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i<words.length; i++){
+  if (words[i].length > 4){
+    return true
+  }
+  return false
+}
 }
 
 function findTheCheese (foods) {
+  for (let i=0 ; i < foods.length; i++){
+    if (foods.includes("cheddar") === true){
+      return "cheddar";
+    }
+    else if (foods.includes("gouda") === true){
+      return "gouda"
+    }
+    else if (foods.includes("camembert") === true){
+      return "camembert"
+    }
+    else {
+      return "no cheese!"
+    }
+  }
 }
